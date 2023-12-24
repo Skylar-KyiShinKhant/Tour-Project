c79ca8e9d36b19914080325e17e0c921f78cbf6d
Skylar <98177468+Skylar-KyiShinKhant@users.noreply.github.com>
2023-12-24 13:15:41 +1100

    Update CreatePackages.java

--- src/TourJava/CreatePackages.java
+++ src/TourJava/CreatePackages.java
@@ -15,7 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
  *
- * @author steve
+ * @author skylar
  */
 public class CreatePackages extends javax.swing.JFrame implements ActionListener{
 
